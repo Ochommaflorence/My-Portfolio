cb5914a3cf7ae123a98abb2eed7e32b589656fc4
chizzy01 <elvischizzy09@gmail.com>
2021-06-22 18:54:08 +0100

    added files

--- src/Components/Pages/Project.js
+++ src/Components/Pages/Project.js
@@ -5,13 +5,26 @@ const Projects = () => {
   return (
     <section className="container" id="projects">
       <h2 className="title">Projects</h2>
-      <div>
+      <div className="row">
+        <div className="col-md-6">
+          <div>
           {projects.map((project)=> {
               const {title, image, technology, description, siteLink, id} = project;
               return(
-              <h1>{title}</h1>
+              <div className="single-box">
+              <img src={image} alt="projectPic" />
+              <span className="content">
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <a>{siteLink}</a>
+              </span>
+                
+
+              </div>
               )
           })}
+      </div>
+      </div>
       </div>
     </section>
   );
